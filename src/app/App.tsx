827b0a9213a8fef313f2de12210b80f979283480
EndlessC0ding <endlesscoding@yahoo.com>
2025-10-24 20:14:01 +0300

    cr sh/cN

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,12 +7,13 @@ import { useTheme } from "./providers/ThemeProvider/useTheme";
 
 import "./App.css";
 import { NotFoundPage } from "../pages/NotFoundPage";
+import { classNames } from "../shared/classNames/classNames";
 
 export function App() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className={`app app_${theme}_theme`}>
+    <div className={classNames(`app`, {}, [`app_${theme}_theme`])}>
       <Link to={"/"}>Home</Link>
       <Link to={"/profile"}>Profile</Link>
 
---
+++ src/shared/classNames/classNames.ts
@@ -0,0 +1,6 @@
+type Mods = Record<string, boolean | string>;
+
+export const classNames = ( cls: string, mods: Mods, additional: string[]): string => {
+  return [cls, ...additional, ...Object.entries(mods).filter(([cls, value]) => Boolean(value)).map(([cls, value]) => cls)].join(" ");
+  // return `${cls} ${additional.join(" ")} ${Object.entries(mods).filter(([cls, value]) => Boolean(value)).map(([cls, value]) => cls)}`;
+};
